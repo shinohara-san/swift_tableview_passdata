87c6a1398941bfe1cd14fc0f1894aa8f831661f8
shinohara-san <someiyoshino0809@gmail.com>
2020-09-06 12:05:05 +0900

    Initial Commit

---
+++ TableViewPassData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPassData
+//
+//  Created by Yuki Shinohara on 2020/09/06.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
